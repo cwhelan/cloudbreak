3cc5addbb5a17783d43455c5b8d832611ff8a6f2
Chris Whelan <cwhelan@gmail.com>
2013-01-24 17:43:05 -0800

    clean up warnings

--- src/test/scripts/applyVariantsToFastaTest.scala
+++ src/test/scripts/applyVariantsToFastaTest.scala
@@ -3,7 +3,7 @@ import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class applyVariantsToFastaTest extends FunSuite {
+class ApplyVariantsToFastaTest extends FunSuite {
 
   test("TwoPlusTwo") {
     val twoPlusTwo = 2 + 2
